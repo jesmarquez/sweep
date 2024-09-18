4968f8ba911e6a25ccbe25209f5e880b40c1f273
DESKTOP-5G35IDM\Jesus Marquez <jesmqz@gmail.com>
2024-09-18 05:00:34 -0400

    Add burned validation signin

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,18 @@
 module.exports = function (app) {
   app.post(
     "/api/auth/signin", (req, res) => {
-      console.log(req.body);
-      res.send('sign in!');
+      const user = req.body;
+      console.log(user);
+
+      if (user.username === 'jesmqz' && user.password === '123456') {
+        return res.status(200).send({
+          id: 900,
+          username: user.username,
+          email: 'jesmqz@gmail.com'
+        })
+      } else {
+        res.status(500).send({ message: 'User not exist or password does not match!'})
+      }
     }
   )
 }
