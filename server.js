c39294b07fe0b7dc7e14540b64935130d095970d
DESKTOP-5G35IDM\Jesus Marquez <jesmqz@gmail.com>
2024-09-17 22:05:29 -0400

    Install cookie session

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
 
 const app = express();
 const PORT = process.env.PORT || 8081;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: "bezkoder-session",
+    keys: ["COOKIE_SECRET"], // should use as secret environment variable
+    httpOnly: true,
+    sameSite: 'strict'
+  })
+);
 
 app.get('/', function(req, res) {
   res.send('Runing api swept!')
